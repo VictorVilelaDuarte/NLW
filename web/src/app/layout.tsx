3c37f3e32c034edd947d064012c0e0393577f0b8
Victor Vilela Duarte <victorvduarte@gmail.com>
2023-05-15 23:38:25 -0300

    fix

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +16,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-    </html>
+    </html> 
   )
 }
